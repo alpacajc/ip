62c59dfa2dd20dc52268ba0ef64a7e03279c7599
Chang Jie <alpamain@outlook.com>
2026-08-26 18:37:16 +0800

    Add Task class, as well as mark and unmark methods to both the task class and the Echo class
    Done to finish Level-3 while following OOP principles like encapsulation.
    
    Changing the list in the TodoList class to hold Tasks instead of just
    strings containing task names. Adding attributes like whether the task
    is done, and task description to the Task class.
    
    This allows polymorphism later when more Task types are added.

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -11,17 +11,60 @@ public class Echo {
     private TodoList todoList = new TodoList();
     private Scanner scanner = new Scanner(System.in);
 
-    private class TodoList {
-        private ArrayList<String> list = new ArrayList<>();
+    public class Task {
+        private String description;
+        private boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+        public String get_desc() {
+            return description;
+        }
+        public String get_status_marker() {
+            return this.isDone ? "[X]" : "[ ]";
+        }
+        public void mark() {
+            this.isDone = true;
+        }
+        public void unmark() {
+            this.isDone = false;
+        }
+        @Override
+        public String toString() {
+            return this.get_status_marker() + this.description;
+        }
+    }
+
+    public class TodoList {
+        private ArrayList<Task> list = new ArrayList<>();
         public void add_to_list(String item) {
-            list.add(item);
+            list.add(new Task(item));
+        }
+        public void mark_list(int taskNum) {
+            if (list.size() < taskNum) {
+                throw new IllegalArgumentException();
+            }
+            list.get(taskNum - 1).mark();
+        }
+        public void unmark_list(int taskNum) {
+            if (list.size() < taskNum) {
+                throw new IllegalArgumentException();
+            }
+            list.get(taskNum - 1).unmark();
+        }
+        public Task getTask(int index){
+            return this.list.get(index);
         }
         @Override
         public String toString() {
             int len = list.size();
             String output = "";
             for (int i = 0; i < len; i ++) {
-                output += String.format("%d. %s\n", i + 1, list.get(i));
+                Task currentElem = list.get(i);
+                output += String.format("%d. %s %s\n", i + 1,
+                        currentElem.get_status_marker(),currentElem.get_desc());
             }
             return LINE + output + ENDLINE;
         }
@@ -46,13 +89,36 @@ public class Echo {
     private void start() {
         while (!endSession) {
             String input = this.scanner.nextLine();
-            if (input.equalsIgnoreCase("bye")) {
+            String command = input.toLowerCase();
+            if (command.equals("bye")) {
                 endSession = true;
                 this.print_farewell();
                 break;
             }
-            else if (input.equalsIgnoreCase("list")) {
+            else if (command.equals("list")) {
                 System.out.println(this.todoList);
+            }
+            else if (command.startsWith("mark ")) {
+                try {
+                    int taskNum = Integer.parseInt(command.substring(5).trim());
+                    this.todoList.mark_list(taskNum);
+                    System.out.println(String.format("Marked this task as done:\n  %s",
+                            todoList.getTask(taskNum - 1)));
+                }
+                catch (IllegalArgumentException e) {
+                    System.out.println("Invalid input for mark. Example usage: mark 2");
+                }
+            }
+            else if (command.startsWith("unmark ")) {
+                try {
+                    int taskNum = Integer.parseInt(command.substring(7).trim());
+                    this.todoList.unmark_list(taskNum);
+                    System.out.println(String.format("Marked this task as not done:\n  %s",
+                            todoList.getTask(taskNum - 1)));
+                }
+                catch (IllegalArgumentException e) {
+                    System.out.println("Invalid input for unmark. Example usage: unmark 2");
+                }
             }
             else {
                 this.todoList.add_to_list(input);
