fe6639b167a77621840c9d9348df0a8537fd90d8
Chang Jie <alpamain@outlook.com>
2026-08-30 23:05:47 +0800

    Add save to text file functionality

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,5 +1,9 @@
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 /**
  * Reads user input, and performs a task based on whether the input matches certain commands,
  * such as adding various tasks to a to do list. Exits when the bye command is entered
@@ -11,47 +15,7 @@ public class Echo {
     private static boolean endSession = false;
     private TodoList todoList = new TodoList();
     private Scanner scanner = new Scanner(System.in);
-
-    public class TodoList {
-        private ArrayList<Task> list = new ArrayList<>();
-        public void addToList(Task item) {
-            list.add(item);
-            System.out.println(String.format("\nThere are now %d items in the list\n",
-                    list.size()));
-        }
-        public void markList(int taskNum) {
-            if (list.size() < taskNum || taskNum < 1) {
-                throw new IllegalArgumentException();
-            }
-            list.get(taskNum - 1).mark();
-        }
-        public void unmarkList(int taskNum) {
-            if (list.size() < taskNum || taskNum < 1) {
-                throw new IllegalArgumentException();
-            }
-            list.get(taskNum - 1).unmark();
-        }
-        public Task deleteTask(int index) {
-            return this.list.remove(index);
-        }
-        public Task getTask(int index){
-            return this.list.get(index);
-        }
-        @Override
-        public String toString() {
-            int len = list.size();
-            String output = "";
-            for (int i = 0; i < len; i ++) {
-                Task currentTask = list.get(i);
-                output += String.format("%d. %s\n", i + 1,
-                        currentTask);
-            }
-            return LINE + output + ENDLINE;
-        }
-        public int getSize() {
-            return list.size();
-        }
-    }
+    private Storage store = new Storage("testdata.txt", todoList);
 
     private void printWelcome() {
         String banner = " _____     _           \n"
@@ -118,6 +82,7 @@ public class Echo {
                             this.todoList.markList(taskNum);
                             System.out.println(String.format("Marked this task as done:\n  %s",
                                     todoList.getTask(taskNum - 1)));
+                            store.writeData(todoList.getList());
                         } catch (IllegalArgumentException e) {
                             System.out.println("Invalid input for mark. Example usage: mark 2");
                         }
@@ -128,6 +93,7 @@ public class Echo {
                             this.todoList.unmarkList(taskNum);
                             System.out.println(String.format("Marked this task as not done:\n  %s",
                                     todoList.getTask(taskNum - 1)));
+                            store.writeData(todoList.getList());
                         } catch (IllegalArgumentException e) {
                             System.out.println("Invalid input for unmark. Example usage: unmark 2");
                         }
@@ -142,6 +108,7 @@ public class Echo {
                             this.todoList.addToList(newTask);
                             System.out.println(String.format("Added this todo task:\n  %s",
                                     newTask));
+                            store.writeData(todoList.getList());
                         } catch (IllegalArgumentException e) {
                             System.out.println("Invalid input for todo.");
                         }
@@ -156,6 +123,7 @@ public class Echo {
                             this.todoList.addToList(newTask);
                             System.out.println(String.format("Added this deadline task:\n  %s",
                                     newTask));
+                            store.writeData(todoList.getList());
                         } catch (IllegalArgumentException e) {
                             System.out.println("Invalid input for deadline.");
                         }
@@ -170,6 +138,7 @@ public class Echo {
                             this.todoList.addToList(newTask);
                             System.out.println(String.format("Added this event task:\n  %s",
                                     newTask));
+                            store.writeData(todoList.getList());
                         } catch (IllegalArgumentException e) {
                             System.out.println("Invalid input for event.");
                         }
@@ -178,9 +147,10 @@ public class Echo {
                         try {
                             int taskNum = Integer.parseInt(command.substring(7).trim());
                             System.out.println(String.format("Deleted this task\n  %s\nNow you have %d tasks left",
-                                    todoList.deleteTask(taskNum - 1), todoList.getSize()));
+                                    todoList.deleteTask(taskNum), todoList.getSize()));
+                            store.writeData(todoList.getList());
                         } catch (IllegalArgumentException e) {
-                            System.out.println("Invalid input for unmark. Example usage: unmark 2");
+                            System.out.println("Invalid input for delete. Example usage: delete 2");
                         }
                     }
                     case INVALID -> {
@@ -197,8 +167,139 @@ public class Echo {
 
     public static void main(String[] args) {
         Echo echo = new Echo();
+        echo.store.readData();
         echo.printWelcome();
         echo.start();
+    }
+}
+
+class Storage {
+    File listFile;
+    TodoList todoList;
+    public Storage(String filePath, TodoList todoList) {
+        this.listFile = new File(filePath);
+        this.todoList = todoList;
+    }
+    public void fileExists() {
+        System.out.println("File exists: " + listFile.exists());
+    }
+    // taskArgs: [type, marked, desc, others...]
+    public void readData() {
+        if (listFile.exists()) {
+            System.out.println(listFile.getName() + " already exists");
+            try {
+                Scanner fileReader = new Scanner(listFile);
+                while (fileReader.hasNext()) {
+                    String nextLine = fileReader.nextLine();
+                    String[] taskArgs = nextLine.split(" // ");
+                    if (taskArgs.length < 3) {
+                        continue;
+                    }
+                    String taskType = taskArgs[0];
+                    String markedString = taskArgs[1];
+                    boolean marked = Boolean.parseBoolean(markedString);
+                    String taskDesc = taskArgs[2];
+                    if (taskType.equals("T")) {
+                        Task currentTask = new Todo(taskDesc);
+                        if (marked) {
+                            currentTask.mark();
+                        }
+                        todoList.addToList(currentTask);
+                    }
+                    else if (taskType.equals("D")) {
+                        String deadline = taskArgs[3];
+                        Task currentTask = new Deadline(taskDesc, deadline);
+                        if (marked) {
+                            currentTask.mark();
+                        }
+                        todoList.addToList(currentTask);
+                    }
+                    else if (taskType.equals("E")) {
+                        String from = taskArgs[3];
+                        String to = taskArgs[4];
+                        Task currentTask = new Event(taskDesc, from, to);
+                        if (marked) {
+                            currentTask.mark();
+                        }
+                        todoList.addToList(currentTask);
+                    }
+                }
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("File not found");
+            }
+        }
+        else {
+            try {
+                listFile.createNewFile();
+                System.out.println("File created: " + listFile.getName());
+            }
+            catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    public void writeData(ArrayList<Task> taskList) {
+        try {
+            FileWriter fw = new FileWriter(listFile);
+            for (Task task : taskList) {
+                fw.write(task.toStorageFormat() + "\n");
+            }
+            fw.close();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class TodoList {
+    private ArrayList<Task> list = new ArrayList<>();
+    private static final String LINE = "\n" + "-".repeat(30) + "\n";
+    private static final String ENDLINE = "\n" + "-".repeat(30);
+
+    public void addToList(Task item) {
+        list.add(item);
+        System.out.println(String.format("\nThere are now %d items in the list\n",
+                list.size()));
+    }
+    public void markList(int taskNum) {
+        if (list.size() < taskNum || taskNum < 1) {
+            throw new IllegalArgumentException();
+        }
+        list.get(taskNum - 1).mark();
+    }
+    public void unmarkList(int taskNum) {
+        if (list.size() < taskNum || taskNum < 1) {
+            throw new IllegalArgumentException();
+        }
+        list.get(taskNum - 1).unmark();
+    }
+    public Task deleteTask(int taskNum) {
+        if (list.size() < taskNum || taskNum < 1) {
+            throw new IllegalArgumentException();
+        }
+        return this.list.remove(taskNum - 1);
+    }
+    public ArrayList<Task> getList() {
+        return this.list;
+    }
+    public Task getTask(int index){
+        return this.list.get(index);
+    }
+    @Override
+    public String toString() {
+        int len = list.size();
+        String output = "";
+        for (int i = 0; i < len; i ++) {
+            Task currentTask = list.get(i);
+            output += String.format("%d. %s\n", i + 1,
+                    currentTask);
+        }
+        return LINE + output + ENDLINE;
+    }
+    public int getSize() {
+        return list.size();
     }
 }
 
@@ -216,6 +317,7 @@ class Task {
     public String getDesc() {
         return description;
     }
+    public String getStatus() {return String.valueOf(isDone);}
     public String getStatusMarker() {
         return this.isDone ? "[X]" : "[ ]";
     }
@@ -224,6 +326,10 @@ class Task {
     }
     public void unmark() {
         this.isDone = false;
+    }
+    public String toStorageFormat() {
+        return String.join(" // ", new String[]{"T", String.valueOf(isDone),
+        description});
     }
     @Override
     public String toString() {
@@ -261,6 +367,11 @@ class Deadline extends Task {
         return String.format("%s%s %s (By: %s)", this.taskMarker, this.getStatusMarker(),
                 super.getDesc(), this.deadline);
     }
+    @Override
+    public String toStorageFormat() {
+        return String.join(" // ", new String[]{"D", super.getStatus(),
+                super.getDesc(), deadline});
+    }
 }
 
 /**
@@ -279,6 +390,11 @@ class Event extends Task {
     public String toString() {
         return String.format("%s%s %s (from: %s to: %s)", this.taskMarker, this.getStatusMarker(),
                 super.getDesc(), this.from, this.to);
+    }
+    @Override
+    public String toStorageFormat() {
+        return String.join(" // ", new String[]{"E", super.getStatus(),
+                super.getDesc(), from, to});
     }
 }
 
