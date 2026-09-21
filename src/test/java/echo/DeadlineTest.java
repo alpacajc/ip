b04a80098ae632dc26afd55ba6ceca9a2dfb38bb
Chang Jie <alpamain@outlook.com>
2026-09-21 20:13:57 +0800

    Edit readme file
    Also renamed some test classes.

--- src/test/java/echo/DeadlineTest.java
+++ src/test/java/echo/DeadlineTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class TaskTest {
+public class DeadlineTest {
     @Test
     public void deadlineTestDate() {
         Deadline deadline = new Deadline("do work", "2023-12-23");
--- src/test/java/echo/ParserTest.java
+++ src/test/java/echo/ParserTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class EchoTest {
+public class ParserTest {
     @Test
     public void commandWordTest1() {
         assertEquals("BYE",
